9e181f658bb706e6f27c79261e81e9af5d0c9c85
Scott <scott.ti312@gmail.com>
2023-07-12 03:41:36 -0700

    Rough about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,24 +1,33 @@
 <section id="about">
-	<div>
-		<h3>About</h3>
+	<div class="about">
+		<h3 id="about-title">About</h3>
 		<p>
-			My first experience in programming was around 2009 when I got my first laptop, a late 2008
-			MacBook. I started by coding small terminal programs in C using Xcode. Back then, I knew I
-			wanted to make games but had no clue how to get started nor how difficult developing games on
-			a Mac would be.
+			Born in Seattle and raised in southern California. My first experience in programming was around 2009 when I got my first laptop, a late 2008 model
+			MacBook. I started by coding small terminal programs in C using Xcode.
+		</p>
+		<p>
+			Since then, I've learned a ton about computer science fundamentals and programming in general. I love learning to use cutting edge technologies, even when they're not very well adopted. This website, for example, is a form of practice to learn Svelte/SvelteKit.
+		</p>
+		<p>
+			I hope to soon to apply my knowledge and personal experience in a professional environment. I'm excited for what lies ahead and ready for any challenge.
 		</p>
 	</div>
-	<div class="skills">
-		<h3>Skills</h3>
-		<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-			galley of type and scrambled it to make a type specimen book. It has survived not only five
-			centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
-			was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-			passages, and more recently with desktop publishing software like Aldus PageMaker including
-			versions of Lorem Ipsum.
-		</p>
+	<div class="links">
+		<h3>Links</h3>
+		<div class="links-list">
+			<a href="https://github.com/scottti312">
+				GitHub scottti312
+			</a>
+			<a href="https://www.linkedin.com/in/scott-ti/">
+				LinkedIn scott-ti
+			</a>
+			<a href="mailto:scott.ti312@gmail.com">
+				Email scott.ti312@gmail.com
+			</a>
+			<p>
+				Discord @__scott
+			</p>
+		</div>
 	</div>
 </section>
 
@@ -28,17 +37,49 @@
 		background-color: color(display-p3 0.693 0.865 0.666);
 		display: flex;
 		width: 100%;
-		height: 500px;
+		height: 1000px;
 		justify-content: center;
-		align-items: center;
     gap: 100px;
+		padding-top: 250px;
+		color: rgb(27, 39, 20);
 	}
 
 	:global(body.dark-mode) section {
 		background-color: color(display-p3 0.23 0.35 0.2);
+		color: rgb(214, 255, 197);
 	}
 
-  .skills {
-    max-width: 500px;
-  }
+	h3 {
+		margin-top: 0;
+		font-weight: 1000;
+		font-size: 2.2em;
+	}
+
+	.about {
+		margin-left: 70px;
+		min-width: 400px;
+	}
+
+	.links-list {
+		display: flex;
+		flex-direction: column;
+		margin-right: 70px;
+		min-width: 300px;
+	}
+
+	.links-list a {
+		margin-bottom: 15px;
+		text-decoration-thickness: 2px;
+	}
+
+	.about p {
+		margin-top: 30px;
+	}
+
+	p, a {
+		max-width: 500px;
+		font-size: 1.3em;
+		font-weight: 600;
+	}
+
 </style>
