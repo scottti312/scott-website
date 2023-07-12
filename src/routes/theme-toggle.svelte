b44a87b898d567444bbbdc038f65f54b28576b22
Scott <scott.ti312@gmail.com>
2023-07-11 19:44:59 -0700

    Toggle theme button

--- src/routes/theme-toggle.svelte
+++ src/routes/theme-toggle.svelte
@@ -1,9 +1,119 @@
 <script>
-  const toggle = () => {
-    window.document.body.classList.toggle('dark-mode');
+  import sun from '$lib/images/sun.svg';
+  import moon from '$lib/images/moon.svg';
+  import { themeControl } from './stores';
+  
+  let transitionDone = true;
+
+  function toggle() {
+    let bodyClass = window.document.body.classList;
+    let header = document.body.getElementsByTagName('header')[0];
+    bodyClass.toggle('dark-mode');
+    $themeControl ? themeControl.beDark() : themeControl.beLight();
+    console.log($themeControl);
+
+    let y = window.scrollY;
+		if (y === 0 && $themeControl) {
+			header.style.backgroundColor = "transparent";
+			header.style.transition = "all 0.3s";
+			header.style.boxShadow = "none";
+			header.style.borderBottom = "none";
+		}
+		if (y === 0 && !$themeControl) {
+			header.style.transition = "all 0.3s";
+			header.style.backgroundColor = "transparent";
+			header.style.borderBottom = "none";
+			header.style.boxShadow = "none";
+		}
+		if (y > 0 && $themeControl) {
+			header.style.transition = "all 0.3s";
+			header.style.backgroundColor = "#ffffff";
+			header.style.boxShadow = "0px -50px 60px 20px rgba(0, 0, 0, 0.75)";
+			header.style.borderBottom = "none";
+		}
+		if (y > 0 && !$themeControl) {
+			header.style.transition = "background-color 0.3s";
+			header.style.backgroundColor = "#181818"
+			header.style.borderBottom = "solid 1px #2a2a2a";
+			header.style.boxShadow = "none";
+		}
+
   };
+
+  function done() {
+    transitionDone = transitionDone ? false : true;
+  }
+
+  /**
+	 * @param {{ keyCode: number; }} e
+	 */
+  function keyDown(e) {
+    if (e.keyCode == 65) {
+      toggle();
+    }
+  }
 </script>
 
-<button on:click={toggle}>
-  Toggle
-</button>
+{#if $themeControl && transitionDone}
+<div
+  on:click={toggle}
+  on:keypress={keyDown}
+  on:outrostart={done}
+  on:outroend={done}
+  role="button"
+  tabindex="0"
+>
+  <img
+  src={sun}
+  class="sun"
+  alt='bright theme'
+  height="25"
+  width="25"
+  >
+</div>
+{/if}
+
+{#if !$themeControl && transitionDone}
+  <div
+  on:click={toggle}
+  on:keypress={keyDown}
+  on:outrostart={done}
+  on:outroend={done}
+  role="button"
+  tabindex="0"
+  >
+  <img
+  src={moon}
+  class="moon"
+  alt='dark theme'
+  height="25"
+  width="25"
+  >
+  </div>
+{/if}
+
+<style>
+  div {
+    cursor: pointer;
+  }
+
+  img.sun {
+    transition: transform 5s ease-out;
+  }
+  img.sun:hover {
+    transform: rotate(360deg) scale(1.2) ;
+  }
+
+  img.moon {
+    transition: transform 1s ease-out;
+  }
+
+  img.moon:hover {
+    transform: scale(1.3) rotate(-10deg);
+  }
+
+	:global(body.dark-mode) img.moon {
+    filter: invert(1)
+	}
+
+</style>
