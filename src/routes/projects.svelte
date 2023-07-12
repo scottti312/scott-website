789671c739a6c9eb23f2f643471b480400b5c010
Scott <scott.ti312@gmail.com>
2023-07-11 19:44:34 -0700

    Projects template

---
+++ src/routes/projects.svelte
@@ -0,0 +1,28 @@
+<section>
+	<div class="projects">
+		<h3>Projects</h3>
+	</div>
+</section>
+
+<style>
+	section {
+		transition: background-color 0.5s;
+		background-color: color(display-p3 1 0.664 0.453);
+		display: flex;
+		width: 100%;
+		height: 500px;
+		justify-content: center;
+		align-items: center;
+		gap: 100px;
+	}
+
+  :global(body.dark-mode) section {
+    background-color: color(display-p3 0.822 0.425 0.249);
+  }
+
+	.projects {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
