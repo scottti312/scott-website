519723b599273051eaebf47de4aec4f065e70c06
Scott <scott.ti312@gmail.com>
2023-10-16 15:16:58 -0700

    Update typewriter message

--- src/routes/scripts/messages.ts
+++ src/routes/scripts/messages.ts
@@ -1,6 +1,6 @@
 // thanks to https://gist.github.com/meain/6440b706a97d2dd71574769517e7ed32
 export const messages = [
-	"Full-Stack Developer",
+	"Software Engineer",
 	"Debugging segmentation fault...",
 	"sudo pacman -Syu",
 	"Running unit tests...",
